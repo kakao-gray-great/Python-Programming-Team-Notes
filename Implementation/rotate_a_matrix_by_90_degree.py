4d8a585349cbc9d728395686acdc8682496a3c2d
oopsys6 <operasystem6@naver.com>
2021-01-17 18:00:45 +0900

    feat: 2차원 배열 90도 회전 알고리즘

---
+++ Implementation/rotate_a_matrix_by_90_degree.py
@@ -0,0 +1,9 @@
+# 2차원 배열 90도 회전 알고리즘
+def rotate_a_matrix_by_90_degree(a):
+    n = len(a)
+    m = len(a[0])
+    result = [[0] * n for _ in range(m)]
+    for i in range(n):
+        for j in range(m):
+            result[j][n - i - 1] = a[i][j]
+    return result
