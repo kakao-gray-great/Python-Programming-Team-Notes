ca11b285f2e9110d402932134e555528d6f0b36d
oopsys6 <operasystem6@naver.com>
2021-01-23 17:06:26 +0900

    퀵 정렬

---
+++ Sort/QuickSort_python.py
@@ -0,0 +1,19 @@
+# 퀵 정렬
+# 시간 복잡도 O(NlogN), 최악의 경우 O(N ** 2)
+
+# 파이썬스러운 퀵 정렬
+array = [5, 7, 9, 0, 3, 1, 6, 2, 4, 8]
+
+def quick_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    pivot = array[0]
+    tail = array[1:]
+
+    left_side = [x for x in tail if x <= pivot]
+    right_side = [x for x in tail if x > pivot]
+
+    return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(array))
